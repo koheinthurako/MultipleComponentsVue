bcd14abb08106fbaebf2d5a8124703aa6981bfe4
koheinthurako <captainthura99@gmail.com>
2024-03-04 00:01:28 +0630

    add items and change ui designs

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,51 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    products: {
-      name: "Water",
-      price: 500
-    }
+    products: [
+      {
+        id: 1,
+        name: "Water",
+        price: 500
+      },
+      {
+        id: 2,
+        name: "Peanut Oil",
+        price: 5000
+      },
+      {
+        id: 3,
+        name: "Rice",
+        price: 6000
+      },
+      {
+        id: 4,
+        name: "Salt",
+        price: 3000
+      },
+      {
+        id: 5,
+        name: "Chicken",
+        price: 3500
+      },
+      {
+        id: 6,
+        name: "Onion",
+        price: 1000
+      },
+      {
+        id: 7,
+        name: "Tea",
+        price: 700
+      },
+      {
+        id: 8,
+        name: "Knife",
+        price: 5000
+      },
+    ]
   },
   getters: {
+    allProducts: (state) => state.products
   },
   mutations: {
   },
---
+++ src/views/ItemView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="cart mt-50 mx-5 d-flex flex-wrap">
+    <!-- <v-card title="Card title" subtitle="Subtitle" text="Water" color="primary" width="300px" style="padding: 10px;">
+      <v-card-actions>
+        <v-btn variant="tonal" width="100%">Click me</v-btn>
+      </v-card-actions>
+    </v-card> -->
+    <div v-for="product in products" :key="product.id">
+      <v-card :title="product.name" :subtitle="product.price" variant="tonal" color="#1867C0" width="300px" style="padding: 10px;">
+        <v-card-actions>
+          <v-btn color="#1867C0" variant="flat" width="100%">Add to Cart</v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "itemView",
+
+    data: () => {
+      // msg: "Test Title"
+    },
+
+    computed: {
+    products() {
+      // Retrieve all products using the getter
+      return this.$store.getters.allProducts;
+    }
+  }
+
+  }
+
+</script>
